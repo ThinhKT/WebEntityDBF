55c69743e689fc0dc8cb5bf43918c7e2fea5d96d
ThinhKT <thinhtm99@gmail.com>
2020-07-01 23:11:29 +0700

    Commit đầu

---
+++ WEB/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+        public ActionResult Product()
+        {
+            return View();
+        }
+        public ActionResult ProductCate()
+        {
+            return View();
+        }
+        public ActionResult ListUser()
+        {
+            return View();
+        }
+        public ActionResult Manager()
+        {
+            return View();
+        }
+        public ActionResult Chart()
+        {
+            return View();
+        }
+        public ActionResult Order()
+        {
+            return View();
+        }
+    }
+}
